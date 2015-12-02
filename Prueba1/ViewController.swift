53129b2dc1e38dcc2dbef4a05b52511d8730ea81
Ana de la Fuente <Anuska72@macbookcfie.local>
2015-12-02 22:26:01 +0100

    Initial Commit

---
+++ Prueba1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba1
+//
+//  Created by Ana de la Fuente on 2/12/15.
+//  Copyright © 2015 Ana de la Fuente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
